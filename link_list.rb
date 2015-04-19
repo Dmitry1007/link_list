ff6596fe0785e004fdfc03165cc8fe3f0e16d977
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-19 15:58:45 -0600

    added test file with push and count tests

--- link_list.rb
+++ link_list.rb
@@ -29,7 +29,6 @@ class Ill
       count
     end
   end
-
 end
 
 class Node
@@ -50,18 +49,3 @@ end
 
 # linky.add_node("c")
 # linky.head
-
-
-
-# ```
-# List -- (head) --> ["hello" | -]-- (link) --> ["world" | -]-- (link) --> ["!" | ]
-# ```
-# The three nodes here hold the data "hello", "world", and "!". 
-# The first two node have links which point to other nodes. 
-# The last node, holding the data "!", has no reference in the link spot. 
-# This signifies that it is the end of the list.
-
-#   Recursive solutions use methods which call themselves to walk through nodes. 
-# * Most of your methods will be defined on the `List` itself, but you will need to manipulate one or more `Node`s to implement them. 
-# * An __empty__ list has `nil` as its head
-# * The __tail__ of a list is the node that has `nil` as its next node
